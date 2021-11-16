d3a8d1f8781bc887f190fb3c31e4b0ffbfa67824
Montgomery James Kern <montgomerykern@users.noreply.github.com>
2021-11-15 22:55:45 -0500

    moved

--- app/main.js
+++ app/main.js
@@ -19,7 +19,6 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 }
 
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
