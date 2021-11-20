c25a529d588db3017a7d5bb7856b901620851390
Montgomery James Kern <montgomerykern@users.noreply.github.com>
2021-11-19 22:50:49 -0500

    code cleanup and mem test

--- app/main.js
+++ app/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 650,
-    height: 550,
+    width: 600,
+    height: 580,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
